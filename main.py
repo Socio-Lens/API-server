99cae776eed4d7edf14b2f9da41aa02a64e9055a
itsAnanth <ananthsankar2003@gmail.com>
2025-11-02 16:27:04 +0530

    feat: health page skeleton

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import os
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 parser = argparse.ArgumentParser(description='SocioLens Web Server')
 parser.add_argument('-n', '--num-gpus', type=int, default=1, help='Number of GPUs to use (default: 1)')
@@ -9,6 +11,19 @@ parser.add_argument('-p', '--port', type=int, default=8000, help='Port to run th
 parser.add_argument('--host', type=str, default='0.0.0.0', help='Host to bind the server to')
 parser.add_argument('--debug', action='store_true')
 args = parser.parse_args()
+
+templates = Jinja2Templates(directory="content")
+
+SERVICES = {
+    "SocioLens API": "http://localhost:8000/",
+    "GitHub": "https://api.github.com",
+    "Google": "https://www.google.com",
+    "FastAPI": "https://fastapi.tiangolo.com"
+}
+
+# Health state
+health_data = {name: {"url": url, "status": "Unknown", "last_checked": None} for name, url in SERVICES.items()}
+
 
 logging.basicConfig(
     level=logging.DEBUG if args.debug else logging.INFO,
@@ -22,6 +37,17 @@ logger = logging.getLogger(__name__)
 
 
 app = FastAPI()
+
+@app.get("/health")
+def health(request: Request, response_class=HTMLResponse):
+    return templates.TemplateResponse("health.html", {"request": request, "services": health_data})
+    
+    
+@app.get("/pid")
+def pid():
+    return {
+        "pid": os.getpid()
+    }
 
 
 @app.get("/")
