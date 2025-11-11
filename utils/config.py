0e736739f25fc3224b47e011ac2e8a91f525a41c
itsAnanth <ananthsankar2003@gmail.com>
2025-11-11 17:24:20 +0530

    feat: added config dataclass

--- utils/config.py
+++ utils/config.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-   pass 
+
+   model_dir: str = 'models'
+   model_name: str = "cardiffnlp/twitter-roberta-base-sentiment-latest"
     
     
